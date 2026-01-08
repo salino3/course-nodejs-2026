8e4f025b86d715e1ac588edfcd294c02492fee94
Salino3 <flaviobcn012@gmail.com>
2026-01-08 21:01:10 +0200

    save changes

---
+++ system-info.js
@@ -0,0 +1,12 @@
+import os from "node:os";
+
+console.log("OS: ", os.type());
+console.log("Platform: ", os.platform());
+console.log("Architcture: ", os.arch());
+console.log("Total meomory (bytes): ", os.totalmem());
+console.log("Free memory (bytes): ", os.freemem());
+console.log("Directory user: ", os.homedir());
+console.log("Time activity (Seconds): ", os.uptime());
+console.log("CPUS: ", os.cpus());
+console.log("Network interface: ", os.networkInterfaces());
+console.log("--------------------");
