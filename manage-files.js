8ae73fc40ba3e5559a72c9145ed606864c680d03
Salino3 <flaviobcn012@gmail.com>
2026-01-08 20:41:32 +0200

    save changes

--- manage-files.js
+++ manage-files.js
@@ -1,5 +1,14 @@
-import { readFile } from "node:fs/promises";
+import { mkdir, readFile, writeFile } from "node:fs/promises";
 
 const contentReadFile = await readFile("./archivo.txt", "utf-8");
 
 console.log("contentReadFile: ", contentReadFile);
+
+const outputDir = "output/files/documents";
+// with 'recursive' it makes the folder even if not exist the
+// first folder and it makes the nexts folder if they don't exist
+await mkdir(outputDir, { recursive: true });
+
+const upperCaseContent = contentReadFile.toUpperCase();
+await writeFile(`./${outputDir}/archivo-uppercase.txt`, upperCaseContent);
+console.log("upperCaseContent: ", upperCaseContent);
