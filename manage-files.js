b8ac235ee9fcccfbd6aba2562fe517b1416056f2
Salino3 <flaviobcn012@gmail.com>
2026-01-08 20:48:19 +0200

    save changes

--- manage-files.js
+++ manage-files.js
@@ -1,10 +1,17 @@
 import { mkdir, readFile, writeFile } from "node:fs/promises";
+import { join } from "node:path";
 
+// slash '/' optional
 const contentReadFile = await readFile("./archivo.txt", "utf-8");
 
 console.log("contentReadFile: ", contentReadFile);
 
-const outputDir = "output/files/documents";
+//* Version 1
+// const outputDir = "output/files/documents";
+
+//* Version 2 with 'join' for every operation system, (slashes folders)
+const outputDir = join("output", "files", "documents");
+
 // with 'recursive' it makes the folder even if not exist the
 // first folder and it makes the nexts folder if they don't exist
 await mkdir(outputDir, { recursive: true });
